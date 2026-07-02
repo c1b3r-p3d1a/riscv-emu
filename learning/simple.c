02d885fba0616d8265f76dc05282dae31bb25fca
c1b3r-p3d1a <100363165+c1b3r-p3d1a@users.noreply.github.com>
2026-07-02 02:11:19 +0200

    0.0.1

--- learning/simple.c
+++ learning/simple.c
@@ -7,19 +7,21 @@ typedef struct {
 	uint8_t memoria[1024];
 } CPU;
 
-int main(void) {
-	CPU cpu;
-	int indice = 0;
-
-	if ((indice != 0)) {
-		cpu.registros[indice] = 100;
-		indice += 1;
+void ejecutar_add(CPU *cpu, int rd, int rs1, int rs2) {
+	if (rd != 0) {
+		cpu->registros[rd] = cpu->registros[rs1] + cpu->registros[rs2];
 	} else {
-		cpu.registros[0] = 0;
-		indice += 1;
+		cpu->registros[rd] = 0;
 	}
+}
 
-	printf("%u\n%u\n", cpu.registros[0], cpu.registros[5]);
+int main(void) {
+	CPU cpu = {0};
+	cpu.registros[1] = 10;
+	cpu.registros[2] = 32;
+	ejecutar_add(&cpu, 3, 1, 2);
+	ejecutar_add(&cpu, 0, 1, 2);
+	printf("%u\n%u\n", cpu.registros[3], cpu.registros[0]);
 
 	return 0;
 }
